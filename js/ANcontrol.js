ad8da49fe7099c427874d8063fd791ec74b7ba82
q123wer2002 <q123wer2002@gmail.com>
2014-10-31 18:14:33 +0800

    yenchen

--- js/ANcontrol.js
+++ js/ANcontrol.js
@@ -9,3 +9,33 @@ TaoLou.controller('TaoLouMenu',['$scope', function TaoLouMenu($scope){
 		{'name':'登入','url':'login.html'}
 	];
 }]);
+
+TaoLou.controller('TaoLoujob-index',['$scope', function TaoLoujobIndex($scope){
+
+	$scope.hotJobs=[
+		{'name':'全部','hotTag':'1'},
+		{'name':'產品經理','hotTag':'2'},
+		{'name':'前端工程師','hotTag':'3'},
+		{'name':'後端工程師','hotTag':'4'},
+		{'name':'iOS工程師','hotTag':'5'},
+		{'name':'Android工程師','hotTag':'6'},
+		{'name':'設計師','hotTag':'7'},
+	];
+
+	$scope.positions=[
+		{'name':'全部','posTag':'1'},
+		{'name':'北部','posTag':'2'},
+		{'name':'中部','posTag':'3'},
+		{'name':'南部','posTag':'4'},
+		{'name':'東部','posTag':'5'},
+		{'name':'外海','posTag':'6'}
+	];
+
+	$scope.jobTypes=[
+		{'name':'全部','typeTag':'1'},
+		{'name':'正職','typeTag':'2'},
+		{'name':'兼職','typeTag':'3'},
+		{'name':'實習','typeTag':'4'}
+	];
+
+}]);
---
+++ js/layout.js
@@ -0,0 +1,21 @@
+
+var fixmeTop = '460'; // Get initial position
+$(window).scroll(function() {            // Assign scroll event listener
+    var currentScroll = $(window).scrollTop(); // Get current position
+    if (currentScroll >= fixmeTop) { // Make it fixed if you've scrolled to it
+        $('.taoloumenu').css({
+            position: 'fixed',
+            height:'70px',
+            top: '0px',
+            left: '0px'
+        });
+    }
+    else{
+        $('.taoloumenu').css({
+            position: 'relative',
+            height:'70px',
+            top: '0px',
+            left: '0px',
+        });
+    }
+});
