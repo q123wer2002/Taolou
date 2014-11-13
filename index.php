37b3715d1b210465d2d2fa88fa386cd8b2e4f4db
yenchen <q123wer2002@gmail.com>
2014-11-13 13:26:25 +0800

    index_complete

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ include_once 'share.php';
 //page default
 $obj_tmp1->jobtable="taolou_job";
 $obj_tmp1->companyTable="taolou_company";
+$obj_tmp1->companySkill='taolou_company_skill';
 $obj_tmp1->tmp_where="";
+$obj_tmp1->page="";
+$obj_tmp1->max_page="";
 $obj_tmp1->laout_set=true;
 $obj_tmp1->tmp_order ='order By sort Asc';
 
-
+//分頁顯示設定
+if(laout_check($_REQUEST['page'])==''){$obj_tmp1->page='1';}
+else {$obj_tmp1->page=laout_check($_REQUEST['page']);}
+$jobshow_start=($obj_tmp1->page-1)*20;
+//=============
 
 switch($action){
 	default:
@@ -26,17 +33,46 @@ switch($action){
 
 	//職位列表
 		//==篩選器
-	$obj_tmp1->tmp_where="";
+	$obj_tmp1->tmp_where=" WHERE ".$obj_tmp1->jobtable.".status='y'";
 
 		//==========================
 
 		//職位顯示
-	$sql_showJob="SELECT ".$obj_tmp1->jobtable.".* 
+	$sql_showJob="SELECT distinct ".$obj_tmp1->jobtable.".*,
+				  ".$obj_tmp1->companyTable.".companyName,".$obj_tmp1->companyTable.".logo,".$obj_tmp1->companyTable.".memberSize
 				  FROM ".$obj_tmp1->jobtable."
-				  LEFT JOIN ".$obj_tmp1->companyTable." ON ".$obj_tmp1->companyTable.".id=".$obj_tmp1->companyTable.".companyId 
-				  ".$obj_tmp1->tmp_where;
+				  LEFT JOIN ".$obj_tmp1->companyTable." ON ".$obj_tmp1->companyTable.".id=".$obj_tmp1->jobtable.".companyId 
+				  ".$obj_tmp1->tmp_where."
+				  ORDER BY ".$obj_tmp1->jobtable.".createDate
+				  LIMIT ".$jobshow_start.",20";
+    $obj_tmp1->laout_arr['showJob']=array();
     $obj_tmp1->basic_select('laout_arr','showJob',$sql_showJob);
+    	//echo $sql_showJob;
+		//print_r($obj_tmp1->laout_arr['showJob']);
+
+    	//公司技能標籤
+    /*$sql_comSkill="SELECT ".$obj_tmp1->companySkill.".*
+				   FROM ".$obj_tmp1->companySkill."
+				   LEFT JOIN ".$obj_tmp1->companyTable." ON ".$obj_tmp1->companySkill.".companyId=".$obj_tmp1->companyTable.".id
+				   WHERE ".$obj_tmp1->companySkill.".companyId=".$obj_tmp1->jobtable.".companyId";
+	$obj_tmp1->laout_arr['comskill']=array();
+	$obj_tmp1->basic_select('laout_arr','comskill',$sql_comSkill);*/
+		//echo $sql_comSkill;
+		//print_r($obj_tmp1->laout_arr['showJob']);
 	   //===========================
+
+		//分頁設定
+	$sql_count="SELECT COUNT(".$obj_tmp1->jobtable.".id) as COUNT
+				FROM ".$obj_tmp1->jobtable."
+				LEFT JOIN ".$obj_tmp1->companyTable." ON ".$obj_tmp1->companyTable.".id=".$obj_tmp1->jobtable.".companyId 
+				".$obj_tmp1->tmp_where."";
+	$obj_tmp1->laout_arr['jobcount']=array();
+    $obj_tmp1->basic_select('laout_arr','jobcount',$sql_count);
+    	
+    $obj_tmp1->max_page=ceil($obj_tmp1->laout_arr['jobcount'][0]['COUNT']/20);
+    	//echo $obj_tmp1->max_page;
+		//==========================
+	
 
 
     //echo $obj_tmp1->encode("1"),"<BR>";
