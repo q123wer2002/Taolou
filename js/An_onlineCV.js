c59d182bb3530aa38c082c51cb205fb8184da484
q123wer2002 <q123wer2002@gmail.com>
2014-11-16 16:43:00 +0800

    userMe_part2

---
+++ js/An_onlineCV.js
@@ -0,0 +1,25 @@
+TaoLou.controller('Taolou_onlineCV',['$scope','$http',function onlineCV($scope,$http){
+	
+	$scope.phone=[
+		{'phone':'886911400733'},
+	];
+	
+	$scope.job_wish=[
+		{'name':'Android',
+		'type1':'y','type2':'y','type3':'y',
+		'min_salary':'200000',
+		'stock_option':'y',
+		'loc':'Taiwan',
+		'far_work':'y'},
+	];
+
+	$scope.edit = function(item) {
+		item.editing=true;
+	}
+	$scope.save = function(item) {
+		delete item.editing;
+	}
+
+
+
+}]);
