44f8cb0a26f3c0f610588bbd9af5cc79dbf96abb
q123wer2002 <q123wer2002@gmail.com>
2014-12-08 20:57:17 +0800

    userMe_Photo

--- js/An_onlineCV.js
+++ js/An_onlineCV.js
@@ -431,10 +431,39 @@ TaoLou.filter('range', function() {
 
 
 //一般的Javascript
+ function handleFiles(files) {
+      var d = document.getElementById("fileList");
+      if (!files.length) {
+        d.innerHTML = "<p>No files selected!</p>";
+      } else {
+        var list = document.createElement("ul");
+        d.appendChild(list);
+        for (var i=0; i < files.length; i++) {
+          var li = document.createElement("li");
+          list.appendChild(li);
+          
+          var img = document.createElement("img");
+          img.src = window.URL.createObjectURL(files[i]);;
+          img.height = 60;
+          img.onload = function() {
+            window.URL.revokeObjectURL(this.src);
+          }
+          li.appendChild(img);
+          
+          var info = document.createElement("span");
+          info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+          li.appendChild(info);
+        }
+      }
+    }
+
+
+
 $(document).ready(function(){
 	//點擊上傳檔案
 	$('.avatar').click(function(){
-		$('#uploadfile').click();
+		var el = document.getElementById("uploadfile");
+      	if (el) {el.click();}
 	});
 
 	$('.editProfile').click(function(){
