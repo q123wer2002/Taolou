695b88d084509d218427d42614c7552702d1f30a
q123wer2002 <q123wer2002@gmail.com>
2014-12-13 13:09:06 +0800

    update

---
+++ companyPost.php
@@ -0,0 +1,57 @@
+<?php
+include_once 'share.php';
+
+//page default
+
+$obj_tmp1->member='taolou_member_detail';
+
+$obj_tmp1->tmp_where="";
+$obj_tmp1->laout_set=true;
+$obj_tmp1->tmp_order ='order By sort Asc';
+
+//確認使用者是誰
+if(@laout_check($_GET['action']) != ""){$getAction=laout_check($_GET['action']);}else{$getAction="";}
+if(@$_SESSION['user']['id']!= ""){$userId=$_SESSION['user']['id'];}else{@$action='none';}
+if(@$_SESSION['user']['userType'] != "" && @$getAction==""){
+    if(@$_SESSION['user']['userType'] == '1'){$action='none';}
+    else if(@$_SESSION['user']['userType'] == '2'){$action='postJob';}
+    else{$action='none';}
+}else{$action=laout_check($_GET['action']);}
+//===================
+
+
+switch(@$action){
+	case "postJob":
+
+
+	$obj_tmp1->showad=false;
+    $obj_tmp1->content_html='content/company/postJob.html';
+    //$obj_tmp1->subMenu='content/user/MenuuserSetting.html';
+
+    //設定版面
+    $obj_tmp1->top_html="top.html";
+	$obj_tmp1->showad_html='showad.html';
+    $obj_tmp1->footer_html="footer.html";
+    $obj_tmp1->laout('templates.html');
+//=======================================
+
+	break;
+
+
+	default:
+
+	$obj_tmp1->showad=false;
+    $obj_tmp1->content_html='content/404.html';
+
+    //設定版面
+    $obj_tmp1->top_html="top.html";
+	$obj_tmp1->showad_html='showad.html';
+    $obj_tmp1->footer_html="footer.html";
+    $obj_tmp1->laout('templates.html');
+//=======================================
+
+	break;
+}
+
+
+?>
